e08c5ba33348e07edecb7f1fb789d2d5c6dece78
Brian Dant <briandant414@gmail.com>
2017-06-20 14:19:55 -0700

    Rely on edX's runtime to show notifications
    edX has a nice (though poorly documented!) APIs
    for showing notifications to the user, which we've used
    in the past and now return to in an expanded way.
    
    Note that this commit is dependent upon a forthcoming
    PR to the edX platform.
    
    Other:
    
    This also maps the escape key to runtime cancel.

--- launchcontainer/static/js/src/launchcontainer_edit.js
+++ launchcontainer/static/js/src/launchcontainer_edit.js
@@ -1,51 +1,53 @@
 function LaunchContainerEditBlock(runtime, element) {
 
-    console.log($('.xblock-save-button', element));
-
-    function notify(response) {
-        var $notificationEl = $('#notification-message'); 
-        var $editModal = $('.wrapper.wrapper-modal-window.wrapper-modal-window-edit-xblock');
-        var $editModalOverlay = $('.modal-window-overlay');
-        var $displayHeader = $('.xblock-header.xblock-header-launchcontainer .header-details');
-        var $editSuccessContent = "Your changes have been successfully applied. "
-                                  + "Please refresh and then click the button above to test.";
-        var $launcherNotification = $('#launcher_notification');
-
-        if (response.result === 'success') {
-          // class="ui-loading is-hidden"
-          
-          // Hide the modal in the Studio interface.
-//          $notificationEl.find('span').html('Your request was successful.');
-          $editModal.addClass('is-hidden');
-          $editModalOverlay.addClass('is-hidden');
-          $launcherNotification.text($editSuccessContent);
-          $launcherNotification.addClass('ui-state-highlight')
-                               .removeClass('ui-state-error')
-                               .removeClass('hide');
-          $('#launcher_form').removeClass('hide');
-        }
-        else {
-          $notificationEl.addClass('error').removeClass('hide').removeClass('is-hidden');
-          $notificationEl.html("It didn't work! "+response.result); 
-        }
-    }
-
+    // Handle the save button click.
     $('.save-button', element).bind('click', function() {
         var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
         var data = {
-            'project': $('#project_input').val(),
-            'project_friendly': $('#project_friendly_input').val(),
-            'project_token': $('#project_token_input').val(),
+          'project': $('#project_input').val(),
+          'project_friendly': $('#project_friendly_input').val(),
+          'project_token': $('#project_token_input').val()
         };
 
         $.post(handlerUrl, JSON.stringify(data))
           .done(function(response) { 
-              notify(response);
-        });
+            if (response.result === 'success') {
+            // Update the hidden form and the button text to allow the 
+            // user to submit to AVL w/o refreshing.
+            // TODO: Can we rely on the RefreshXBlock function 
+            // directly to better control this experience? https://git.io/vQIPs
+            $('#launcher_project').val(response.validated_data.project);
+            $('#launcher_token').val(response.validated_data.project_token);
+            $('#launcher_submit').text("Click to launch your " + response.validated_data.project_friendly + "lab");
+
+            // Notify the user of success.
+            // This is only available in the Appsembler fork of Open edX, though it 
+            // will fail silently elsewhere.
+            runtime.notify('save-and-confirm', {
+              title: 'Success!',
+              message: 'Your AVL project data has been saved. ' 
+                + 'Please click the launch button to test.', 
+              closeIcon: true, 
+              maxShown: 4000
+            });
+
+            } else {
+              runtime.notify('error', {message: 'Error: '+response.result});
+            }
+          });
     });
 
+    // Handle the cancel button click.
     $('.cancel-button', element).bind('click', function() {
         runtime.notify('cancel', {});
     });
+
+    // Allow the escape key to close the XBlock form.
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) {
+        runtime.notify('cancel');
+      }
+    });
 }
+
 
