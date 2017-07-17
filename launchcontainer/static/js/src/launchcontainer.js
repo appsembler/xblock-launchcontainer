d8dcce569db4cfc23c1a01a95258c0f01a4eacb3
Brian Dant <briandant414@gmail.com>
2017-07-16 17:42:59 -0700

    Initial (untested) attempt to make the error handling better

--- launchcontainer/static/js/src/launchcontainer.js
+++ launchcontainer/static/js/src/launchcontainer.js
@@ -11,38 +11,58 @@ function LaunchContainerXBlock(runtime, element) {
   $(document).ready(
     function () {
 
-      var _show_notification = function ($type, $title, $message, $html) {
+      var _show_notification = function (type, params) {
         // Toggle the message that is shown to the user. 
-        $message = $message || undefined; 
-        $html = $html || undefined;
-        var $iconMap = {'error': 'warning', 'information': 'bullhorn', 'success': 'check'};
+        
+        var title = params.title, 
+            textMessage = params.textMessage || undefined, 
+            htmlMessage = params.htmlMessage || undefined, 
+            errors = params.errors || undefined,
+            iconMap = {
+              'error': 'warning', 
+              'information': 'bullhorn', 
+              'success': 'check'
+            };
+
         // Segments of a class need to be overwritten, so I just drop all the classes instead
         // of e.g. a regex. 
         // attr() is used instead of removeClass() because a bug in jQuery UI 1.10.0: 
         // https://bugs.jqueryui.com/ticket/9015 
-        $('div.launch-alert').attr('class', 'launch-alert launch-alert--' + $type); 
-        $('span.launch-alert--icon').attr('class', 'launch-alert--icon fa fa-' + $iconMap[$type]); 
-        $('h3.launch-alert--title').text($title);
+        $('div.launch-alert').attr('class', 'launch-alert launch-alert--' + type); 
+        $('span.launch-alert--icon').attr('class', 'launch-alert--icon fa fa-' + iconMap[type]); 
+        $('h3.launch-alert--title').text(title);
 
-        if ($message) {
-          $('p.launch-alert--body').text($message);
-        } else if ($html) {
+        if (textMessage) {
+          $('p.launch-alert--body').text(textMessage);
+        } else if (htmlMessage) {
           // This html only comes from us and ISC, for now. 
           // In the near future we need to remedy this, though, 
-          // for Tahoe, by returning.
-          $('p.launch-alert--body').html($html);
-        };
+          // for Tahoe, by modifying the Wharf interface to disallow html, 
+          // or use Django to sanitize it?
+          $('p.launch-alert--body').html(htmlMessage);
+        }
+
+        if (errors) {
+          var errorListItems = [], 
+              errorListEl = $('.launch-alert--errorListItem')[0];
+          for (i=0; i<$errors.length; i++) { 
+            errorListItems.append($(errorListEl).clone()
+              .text($errors[i][0] + ": " + $errors[i][1][0])
+              .removeClass('is-hidden')
+            );
+          }
+          $('ul.launch-alert--errorList').append(errorItems).removeClass('is-hidden');
+        }
 
         $('#launcher_notification').removeClass('is-hidden');
-      }
+      };
 
       // Submit the data to the AVL server and process the response.
       var $launcher = $('#launcher1'),
           $post_url = '{{ API_url|escapejs }}',
-          $launcher_form = $('#launcher_form'),
           $launcher_submit = $('#launcher_submit'),
           $launcher_email = $('#launcher_email'),
-          $launch_notification = $('#launcher_notification') 
+          $launch_notification = $('#launcher_notification');
 
       // This is for the xblock-sdk: If there is no email addy, 
       // you can enter it manually.
@@ -59,16 +79,17 @@ function LaunchContainerXBlock(runtime, element) {
 
         // Add notification message.
         _show_notification(
-          'information', 
-          'Your request is being processed', 
-          'This can take up to 90 seconds--thank you for your patience! '
-          + 'If you are having issues, please contact the administrator.'
-        )
+          'information', {
+            title: 'Your request is being processed', 
+            textMessage: 'This can take up to 90 seconds--thank you for your patience! ' + 
+                         'If you are having issues, please contact the administrator.'
+          }
+        );
 
         // Post the message to the iframe.
         $launch_iframe = $launcher.find('iframe')[0];
         $launch_iframe.contentWindow.postMessage({
-          'project': "{{ project|escapejs }}",
+//          'project': "{{ project|escapejs }}",
           'owner_email': "{{ user_email|escapejs }}",
           'token': "{{ project_token|escapejs }}"
           }, "{{ API_url|escapejs }}"
@@ -78,39 +99,53 @@ function LaunchContainerXBlock(runtime, element) {
 
       window.addEventListener("message", function (event) {
         if (event.origin !== getURLOrigin('{{ API_url|escapejs }}')) return;
-        if(event.data.status === 'siteDeployed') {
+        if (event.data.status === 'siteDeployed') {
           _show_notification(
-            'success', 'Success', null,
-            event.data.html_content
-          ); 
-          $launcher_form.addClass('is-hidden');
-        } else if(event.data.status === 'deploymentError') {
-          var $status_code = event.data.status_code;
-          var $msg;
-          if ($status_code === 400) {
-            // xx
-            var $errorList = $("<ul class='launch-alert--errorList'>");
-            var $errors = event.data.errors;
-            for (i=0; i<$errors.length; i++) { 
-              $errorList.append($("<li class='launch-alert--errorListItem'>")
-                                .text($errors[i][0] + ": " + $errors[i][1][0])
-                               ); 
+            'success', {
+              title: 'Success', 
+              htmlMessage: event.data.html_content
             }
-            // xx
-            $msg = $errorList.prop('outerHTML');
-          } else if ($status_code === 403) {
-            $msg = "Your request failed because the token sent with "
-                   + "your request is invalid. "
-          } else if ($status_code === 404) {
-            $msg = "That project was not found. "; 
-          } else if ($status_code === 503) {
-            $msg = event.data.errors + " ";
+          ); 
+          $('.launcher-form--container').addClass('is-hidden');
+        } else if (event.data.status === 'deploymentError') {
+          $('.launcher-form--container').addClass('is-hidden');
+
+          var status_code = event.data.status_code,
+              title = 'Error',
+              errorMessage = "There was an error in your request. Please contact " + 
+                             "the support team, or reload the page and try again.",
+              errors;
+
+          switch (status_code) {
+            case 400:
+            case 503:
+              errors = event.data.errors;
+              break;
+            case 403:
+              // These wonky structures are the way that we match DRF's errors.
+              errors = [
+                [
+                  '', 
+                  [['Your request failed because the token you sent with the request is invalid.']]
+                ]
+              ];
+              break;
+            case 404:
+              errors = [
+                [
+                  '', 
+                  [['That project was not found.']]
+                ]
+              ];
           }
 
           _show_notification(
-            'error', 'Error', 
-            "An error occured in your request: " + $msg + "Please contact the administrator."
-          )
+            'error', { 
+              title: title, 
+              textMessage: errorMessage, 
+              errors: errors 
+            }
+          );
 
         }
       }, false);
