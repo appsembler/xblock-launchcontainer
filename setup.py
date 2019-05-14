6352ce029fdf37f5fac3aa4bed67f57481995c65
Filip Jukić <filip@jukic.me>
2019-05-14 17:21:55 +0200

    add v2.2.2 info

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-launchcontainer',
-    version='2.2.1',
+    version='2.2.2',
     author='Bryan Wilson, Appsembler',
     description=('Open EdX XBlock to display a button allowing an LMS user '
                  'to launch and link to an external courseware resource via the '
